721f3261731a0db4d4f408dc1ddfb0fe56e5bf64
AdamDnd <69721786+AdamDnd@users.noreply.github.com>
2020-08-21 15:47:22 +0100

    5e dice roller

--- src/gp-post.js
+++ src/gp-post.js
@@ -45,34 +45,38 @@ $('.markItUpHeader').on('click','.markItUpAddSnippet',function(){
 
 //forums menu
 var forumsLink=$('#threadMenu .leftCol a').attr('href');
-$.get( 'https://gamersplane.com'+forumsLink, function( data ) {
-    var newMenu=$('#fm_tools');
-    $('.menuLink',newMenu).text('Forums');
-    $('.submenu',newMenu).html('');
-    var dataObj=$(data);
-    var forums=$('.forumList .tr',dataObj);
-    var subMenu=$('.submenu',newMenu);
-    if(forums.length>0)
-    {
-        $('h3',$('<li><h3></h3></li>').appendTo(subMenu)).text('Forums');
-        for(var i=0;i<forums.length;i++)
+if(forumsLink)
+{
+    $.get( 'https://gamersplane.com'+forumsLink, function( data ) {
+        var newMenu=$('#fm_tools');
+        $('.menuLink',newMenu).text('Forums');
+        $('.submenu',newMenu).html('');
+        var dataObj=$(data);
+        var forums=$('.forumList .tr',dataObj);
+        var subMenu=$('.submenu',newMenu);
+        if(forums.length>0)
         {
-            $('<li></li>').html($('.name',forums[i]).html()).appendTo(subMenu);
+            $('h3',$('<li><h3></h3></li>').appendTo(subMenu)).text('Forums');
+            for(var i=0;i<forums.length;i++)
+            {
+                $('<li></li>').html($('.name',forums[i]).html()).appendTo(subMenu);
+            }
         }
-    }
-    var threads=$('.threadList .tr',dataObj);
-    if(threads.length>0)
-    {
-        $('h3',$('<li><h3></h3></li>').appendTo(subMenu)).text('Threads');
-        for(var i=0;i<threads.length;i++)
+        var threads=$('.threadList .tr',dataObj);
+        if(threads.length>0)
         {
-            var menuItem=$('<li><a></a></li>').appendTo(subMenu);
-            var threadText=$('.td.threadInfo>a',threads[i]);
-            $('a',menuItem).attr('href',threadText.attr('href')).text(threadText.text());
+            $('h3',$('<li><h3></h3></li>').appendTo(subMenu)).text('Threads');
+            for(var i=0;i<threads.length;i++)
+            {
+                var menuItem=$('<li><a></a></li>').appendTo(subMenu);
+                var threadText=$('.td.threadInfo>a',threads[i]);
+                $('a',menuItem).attr('href',threadText.attr('href')).text(threadText.text());
+            }
+    
         }
-
-    }
-
-  });
+    
+      });
+    
+}
 
   
