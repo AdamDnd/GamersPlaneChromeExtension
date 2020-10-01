316feb817c035a59ae55841ee3e97b332fab839a
AdamDnd <69721786+AdamDnd@users.noreply.github.com>
2020-10-01 13:11:24 +0100

    One page view.  Savage world fix.

--- src/gp-search.js
+++ src/gp-search.js
@@ -1,67 +1,94 @@
-var searchButton=$('<a id="postSearch">Search</a>').prependTo($('#threadMenu .rightCol'));
+var searchButton = $('<a id="postSearch">Search</a>').prependTo($('#threadMenu .rightCol'));
+$('<span"> | </span>').prependTo($('#threadMenu .rightCol'));
+var singlePageButton = $('<a id="postSinglePage">One page</a>').prependTo($('#threadMenu .rightCol'));
 
-var getPostBlocks=function(basePage,pageNum,searchFor,ret,onComplete)
-{
-    $.get( basePage+'?page='+pageNum, function( data ) {
-        var pageData=$(data);
-        $('.postBlock',pageData).each(function(){
-            var block=$(this);
-            if(($('.post',block).html().toLowerCase().indexOf(searchFor)!=-1) ||
-               ($('.posterDetails',block).html().toLowerCase().indexOf(searchFor)!=-1))
-            {
-                ret.push(block.clone());
-            }
-        });
+var getPostBlocks = function (basePage, pageNum, onLoadingPage, onLoadedPosts, onComplete) {
+    onLoadingPage(pageNum);
 
-        var hasMorePages=false;
-        var pagination=$('.paginateDiv',pageData);
-        if(pagination.length>0)
-        {
-            var nextPage=$('a',pagination).filter(function() {return $(this).text() === ">";});
-            if(nextPage.length>0)
-            {
-                hasMorePages=true;
+    $.get(basePage + '?page=' + pageNum, function (data) {
+        var pageData = $(data);
+        onLoadedPosts($('.postBlock', pageData));
+
+        var hasMorePages = false;
+        var pagination = $('.paginateDiv', pageData);
+        if (pagination.length > 0) {
+            var nextPage = $('a', pagination).filter(function () { return $(this).text() === ">"; });
+            if (nextPage.length > 0) {
+                hasMorePages = true;
             }
         }
-        if(hasMorePages)
-        {
-            searchButton.text('Searching ('+(pageNum+1)+')...');
-            getPostBlocks(basePage,pageNum+1,searchFor,ret,onComplete)
+        if (hasMorePages) {
+            getPostBlocks(basePage, pageNum + 1,onLoadingPage, onLoadedPosts, onComplete);
         }
-        else
-        {
-            onComplete(ret);
+        else {
+            onComplete();
         }
-    });    
+    });
 }
 
-searchButton.on('click',function(){
-    var searchFor=prompt("Text to find", "");
-    if(searchFor!=null && searchFor.length>0)
-    {
+searchButton.on('click', function () {
+    var searchFor = prompt("Text to find", "");
+    if (searchFor != null && searchFor.length > 0) {
+        searchFor = searchFor.toLowerCase();
+        var ret = [];
+
         $('.postBlockFound').remove();
-        var basePage=window.location.href.split('?')[0];
-        searchButton.text('Searching (1)...');
-        var postBlocks=getPostBlocks(basePage,1,searchFor.toLowerCase(),[],function(ret){
-            if(ret.length>0)
-            {
+        $('.postBlock').show();
 
-                var threadMenu=$('#threadMenu');
-                for(var i=ret.length-1;i>=0;i--)
-                {
-                    var addedBlock=($(ret[i]).insertAfter(threadMenu)).addClass('postBlockFound');
-                    if(i==(ret.length-1))
-                    {
-                        addedBlock.addClass('lastPostBlockFound');
+        var basePage = window.location.href.split('?')[0];
+        var postBlocks = getPostBlocks(basePage, 1,
+            function (pageNum) {
+                searchButton.text('Searching (' + pageNum + ')...');
+            },
+            function (blocks) {
+                blocks.each(function () {
+                    var block = $(this);
+                    if (($('.post', block).html().toLowerCase().indexOf(searchFor) != -1) ||
+                        ($('.posterDetails', block).html().toLowerCase().indexOf(searchFor) != -1)) {
+                        ret.push(block.clone());
                     }
+                });
 
-                    $('.spoiler>.tag',addedBlock).on('click',function(){$(this).closest('.spoiler').toggleClass('closed');});
+            },
+            function () {
+                if (ret.length > 0) {
+
+                    var threadMenu = $('#threadMenu');
+                    for (var i = ret.length - 1; i >= 0; i--) {
+                        var addedBlock = ($(ret[i]).insertAfter(threadMenu)).addClass('postBlockFound');
+                        if (i == (ret.length - 1)) {
+                            addedBlock.addClass('lastPostBlockFound');
+                        }
+
+                        $('.spoiler>.tag', addedBlock).on('click', function () { $(this).closest('.spoiler').toggleClass('closed'); });
+                    }
                 }
-            }
-            var title=$('<div class="postBlock postRight postAsChar withCharAvatar clearfix postBlockFound"><h1></h1></div>').insertAfter(threadMenu);
-            $('h1',title).text(searchFor+": "+ret.length+' results');
+                var title = $('<div class="postBlock postRight postAsChar withCharAvatar clearfix postBlockFound"><h1></h1></div>').insertAfter(threadMenu);
+                $('h1', title).text(searchFor + ": " + ret.length + ' results');
 
-        searchButton.text('Search');
-        });
+                searchButton.text('Search');
+            });
     }
+});
+
+singlePageButton.on('click', function () {
+    $('.postBlockFound').remove();
+    $('.postBlock').hide();
+    var basePage = window.location.href.split('?')[0];
+    var lastAdded = $('#threadMenu');
+    var postBlocks = getPostBlocks(basePage, 1,
+        function (pageNum) {
+            singlePageButton.text('Loading (' + pageNum + ')...');
+        },
+        function (blocks) {
+            blocks.each(function () {
+                var block = $(this);
+                var addedBlock = (block.clone().insertAfter(lastAdded)).addClass('postBlockFound');
+                lastAdded = addedBlock;
+                $('.spoiler>.tag', addedBlock).on('click', function () { $(this).closest('.spoiler').toggleClass('closed'); });
+            });
+        },
+        function () {
+            singlePageButton.text('One page');
+        });
 });
