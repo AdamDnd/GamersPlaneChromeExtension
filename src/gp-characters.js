bd04d9225415f985391c3d0df148c9a9c5002bda
Adam <adamdnd@devprocess.com>
2021-11-20 06:26:13 +0000

    Removed functionality moved to the site

--- src/gp-characters.js
+++
@@ -1,5 +0,0 @@
-//applyPageStyle($('#notes').text());
-
-//$('.feat_notesLink').on('click',function(){$(this).toggleClass('open');});
-
-$('<div></div>').prependTo('#page_characterSheet').addClass('avatarPreview').css({'background-image':'url(https://gamersplane.com/characters/avatars/'+$('#characterID').val()+'.jpg?'+(new Date().getTime())+')'});
